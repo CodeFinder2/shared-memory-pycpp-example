8fdfdb207694306e435a23f980e549f323c3c1da
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2020-05-05 16:46:52 +0200

    Improve build script (install deps automatically) and run both apps

--- build.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-mkdir -p build
-cd build
-cmake ..
-make -j2
-cd ..
-
-./build/shared_memory_cpp
---
+++ run.sh
@@ -0,0 +1,52 @@
+set -e
+
+# Test for cmake:
+if ! [ -x "$(command -v cmake)" ]; then
+   read -p "cmake not found (required to configure the build)! Install? (y/n) " -n 1 -r
+   echo # move to a new line
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt-get update
+      sudo apt-get install cmake
+   fi
+fi
+
+# Test for python:
+if ! [ -x "$(command -v python)" ]; then
+   read -p "python not found (required to execute the demo app)! Install? (y/n) " -n 1 -r
+   echo # move to a new line
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt-get update
+      sudo apt-get install python
+   fi
+fi
+
+# Test for gcc
+if ! [ -x "$(command -v gcc)" ]; then
+   read -p "gcc not found (required to compile the sources)! Install? (y/n) " -n 1 -r
+   echo # move to a new line
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt-get update
+      sudo apt-get install build-essential
+   fi
+fi
+
+pkgs='qtbase5-dev pyqt5-devd'
+if ! dpkg -s $pkgs >/dev/null 2>&1; then
+   read -p "One or all of {$pkgs} not found (compilation dependencies)! Install? (y/n) " -n 1 -r
+   echo # move to a new line
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt-get update
+      sudo apt-get install $pkgs
+   fi
+fi
+
+# Compile:
+mkdir -p build
+cd build
+cmake ..
+make -j2
+cd ..
+
+# Run:
+./build/shared_memory_cpp &
+./shared_memory.py
